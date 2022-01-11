49ab791ff457928f249acd0974b6ffabb6c03a5b
roope <roope.jarvelainen@gmail.com>
2022-01-11 19:34:00 +0200

    added ROS publisher

--- swarmcon_server.py
+++ swarmcon_server.py
@@ -1,8 +1,16 @@
 import socket
 import time
 
+import rospy
+from std_msgs.msg import String
+
+
 def main():
-    #init TCP Socket
+    # create rospy publisher
+    pub = rospy.Publisher('swarmcon', String, queue_size=10)
+    rospy.init_node('swarmcon')
+
+    # init TCP Socket
     TCP_IP = 'localhost'
     TCP_PORT = 6666
     BUFFER_SIZE = 1024
@@ -10,12 +18,16 @@ def main():
     s.connect((TCP_IP, TCP_PORT))
 
     print("connected to the CosPhi- Topics are Publishing....")
-    while True:
+    while not rospy.is_shutdown():
         payload = s.recv(BUFFER_SIZE).decode()
         robots = payload.split('\n')
+        msg = ""
         for i in range(len(robots)-2):
             _, id, x, y, angle, t = robots[i+1].split()
-            timestamp = time.localtime(int(t) / 1000000.0)
-            print(f"[{time.strftime('%H:%M:%S', timestamp)}]: {id}: ({x}, {y}), {angle}'")
+            msg += f"{id}, {x}, {y}, {angle}"
+            #timestamp = time.localtime(int(t) / 1000000.0)
+            #print(f"[{time.strftime('%H:%M:%S', timestamp)}]: {id}: ({x}, {y}), {angle}'")
+        pub.publish(msg)
+
 
 main()
